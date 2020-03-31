5a93d20f1b4408929ce837e8f98ddb556fada661
AndresCortes Development <andrescortes.development@gmail.com>
2020-03-31 15:40:10 -0500

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+'use strict'
+const cors = require('cors');
+const authRoutes = require('./auth/auth.routes');
+const express = require('express');
+const propierties = require('./config/properties');
+const DB = require('./config/db');
+// init DB
+DB();
+
+const app = express();
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const bodyParserJSON = bodyParser.json();
+const bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
+
+app.use(bodyParserJSON);
+app.use(bodyParserURLEncoded);
+
+app.use(cors());
+
+app.use('/api', router);
+
+authRoutes(router);
+router.get('/', (req, res) => {
+  res.send('Hola mundo desde /');
+});
+app.use(router);
+app.listen(propierties.PORT, () => console.log(`El server prendio en el puerto ${propierties.PORT}`));
