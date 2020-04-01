001d0f74f1215dd308a51c57e2599cf2a93e4b06
Oscar Amado <oscarfamado@gmail.com>
2020-04-01 14:50:30 -0500

    rutas de usuarios

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+'use strict';
+
 const cors = require('cors');
 const authRoutes = require('./auth/auth.routes');
 const userRoutes = require('./user/user.routes');
 const express = require('express');
 const propierties = require('./config/properties');
 const DB = require('./config/db');
-const bodyParser = require('body-parser');
 
 // init DB
 DB();
@@ -12,28 +13,14 @@ DB();
 const app = express();
 const router = express.Router();
 
+const bodyParser = require('body-parser');
 const bodyParserJSON = bodyParser.json();
 const bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
-
 
 app.use(bodyParserJSON);
 app.use(bodyParserURLEncoded);
 
-app.use( (req, res, next) => {
-
-  res.header('Access-Control-Allow-Origin', '*');
-  // todos los metadatos - cookies
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  // todos los métodos http - métodos de petición
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  // Confirmación estricta de los métodos a utilizar
-  res.header('Allow', 'GET, POST, PUT, DELETE, OPTIONS');
-
-  next();
-
-} )
-
-
+app.use(cors());
 
 app.use('/api', router);
 
