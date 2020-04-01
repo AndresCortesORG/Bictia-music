d65f4377a9001744c2fa31006bd66131e73472fd
Oscar Amado <oscarfamado@gmail.com>
2020-04-01 14:50:55 -0500

    instalacion y configuracion de multer

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const dbURL = require('./properties').DB;
 
 module.exports = () => {
-  mongoose.connect(dbURL, { useNewUrlParser: true })
-    .then(() => console.log(`Mongo conectado en ${dbURL}`))
-    .catch(err => console.log(`error desde db.js ${err}`))
+    mongoose.connect(dbURL, {useNewUrlParser: true, useFindAndModify: false})
+        .then(() => console.log(`Mongo conectado en ${dbURL}`))
+        .catch(err => console.log(`error desde db.js ${err}`));
 
-  process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-      console.log(`Mongo esta morido`);
-      process.exit(0)
+    process.on('SIGINT', () => {
+        mongoose.connection.close(() => {
+            console.log(`Mongo esta morido`);
+            process.exit(0);
+        });
     });
-  });
 }
